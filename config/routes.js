0cf726f99e92f2740273985bf9f3a478785dfa3b
Daniel <danielstarling4@gmail.com>
2019-04-05 08:34:43 -0700

    register built

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
-
+const bcrypt = require('bcryptjs');
 const { authenticate } = require('../auth/authenticate');
+const Users = require('../database/dbConfig');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -9,7 +10,23 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  // implement user registration
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 10);
+  creds.password = hash;
+
+  Users('users')
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+      debug('users')
+        .where({ id })
+        .first()
+        .then(user => {
+          const token = generateToken(user);
+          res.status(201).json({ id: user.id, token });
+        })
+        .catch(err => res.status(500).send('error registering user', err));
+    });
 }
 
 function login(req, res) {
@@ -18,7 +35,7 @@ function login(req, res) {
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: 'application/json' }
   };
 
   axios
